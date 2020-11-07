77278e7257950a80d5bf357400a1b6e7dbff40e7
Ruhulamin098 <33164298+Ruhulamin098@users.noreply.github.com>
2020-11-08 04:26:55 +0600

    Yoga
    Responsive, Javascript

---
+++ js/script.js
@@ -0,0 +1,10 @@
+$(".set_bg").each(function () {
+	var bg = $(this).data("setbg");
+	$(this).css("background-image", "url(" + bg + ")");
+});
+
+AOS.init({
+	disable: "mobile",
+	duration: "800",
+	anchorPlacement: "center-bottom",
+});
